99c3f73c79ef738190a2671532e233c5a66dfc4b
phpusr <phpusr@gmail.com>
2020-02-05 13:45:06 +0500

    WR-4: Added tests for status comment

--- app/tests/test_sync_service.py
+++ app/tests/test_sync_service.py
@@ -1,8 +1,9 @@
 import os
+from unittest.mock import patch
 
 from django.test import TestCase
 
-from app.models import Config
+from app.models import Config, Post, Profile
 from app.services import sync_service
 
 
@@ -13,11 +14,18 @@ def create_config():
         sync_seconds=300,
         group_id=88923650,
         group_short_link='',
-        commenting=True,
+        commenting=False,
         comment_access_token=os.getenv('VK_ACCESS_TOKEN'),
         comment_from_group=False,
         publish_stat=False
     )
+
+
+def create_comment_text(status, profile=None):
+    if not profile:
+        profile = Profile(id=347, first_name='Иван')
+    post = Post(status=status, number=22, author=profile)
+    return sync_service._create_comment_text(post, 10, 20)
 
 
 class SyncServiceTests(TestCase):
@@ -25,5 +33,41 @@ class SyncServiceTests(TestCase):
     def setUp(self):
         create_config()
 
-    def test_sync_posts(self):
-        sync_service.sync_posts()
+    def test_create_comment_text(self):
+        """Test that comment text is correct"""
+        self.assertEqual(
+            create_comment_text(Post.Status.SUCCESS),
+            '#22 пробежка: Пост успешно обработан'
+        )
+        self.assertEqual(
+            create_comment_text(Post.Status.ERROR_SUM),
+            '@id347 (Иван), #22 пробежка: Ошибка при сложении, должно быть: 20'
+        )
+        self.assertEqual(
+            create_comment_text(Post.Status.ERROR_PARSE, Profile(id=-792, first_name='Дикий Забег')),
+            '@club792 (Дикий Забег), Ошибка в формате записи, пост не распознан'
+        )
+        self.assertEqual(
+            create_comment_text(Post.Status.ERROR_START_SUM),
+            '@id347 (Иван), #22 пробежка: Ошибка в стартовой сумме, должна быть: 10'
+        )
+        self.assertEqual(
+            create_comment_text(99),
+            '@id347 (Иван), #22 пробежка: Ошибка: Не предусмотренный статус, напишите администратору'
+        )
+
+    def test_not_add_status_comment(self):
+        """Test that status comment don't leave"""
+        with patch('app.services.vk_api_service.add_comment_to_post') as gi:
+            sync_service._add_status_comment(1, 'Status comment')
+            self.assertEqual(gi.call_count, 0)
+
+    def test_add_status_comment(self):
+        """Test that status comment leave"""
+        config = Config.objects.get()
+        config.commenting = True
+        config.save()
+
+        with patch('app.services.vk_api_service.add_comment_to_post') as gi:
+            sync_service._add_status_comment(1, 'Status comment')
+            self.assertEqual(gi.call_count, 1)
