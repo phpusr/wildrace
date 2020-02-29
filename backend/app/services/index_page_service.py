f4793ac685807cbaa3cf9bb4a7f562fd458d9740
phpusr <phpusr@gmail.com>
2020-02-29 20:16:31 +0500

    WR-12: Hide Google Analytics in debug mode

--- backend/app/services/index_page_service.py
+++ backend/app/services/index_page_service.py
@@ -22,6 +22,7 @@ def get_data(user):
     })
 
     data = {
+        'debug': settings.DEBUG,
         'google_analytics_id': settings.GOOGLE_ANALYTICS_ID,
         'frontend_data': CamelCaseJSONRenderer().render(serializer.data).decode('utf-8')
     }
