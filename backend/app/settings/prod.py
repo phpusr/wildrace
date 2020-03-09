936c222cfdcdf2f085cfa67b9167da94ba70da26
phpusr <phpusr@gmail.com>
2020-03-09 19:05:18 +0500

    WR-23: Added sentry to backend

--- backend/app/settings/prod.py
+++ backend/app/settings/prod.py
@@ -1,6 +1,8 @@
 import os
 
 import dj_database_url
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 from .dev import *  # noqa: F401,F403
 
@@ -12,3 +14,9 @@ DB_URL = os.getenv('DATABASE_URL', 'postgres://127.0.0.1:5432/wildrace')
 DATABASES = {
     'default': dj_database_url.parse(DB_URL)
 }
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_BACKEND_DSN', ''),
+    integrations=[DjangoIntegration()],
+    send_default_pii=True
+)
