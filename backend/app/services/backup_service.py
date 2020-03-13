61d2b508ef9c5d1d56820d1214fb755fef485101
phpusr <phpusr@gmail.com>
2020-03-13 18:24:15 +0500

    WR-32: Added delete old backup files

--- backend/app/services/backup_service.py
+++ backend/app/services/backup_service.py
@@ -11,12 +11,20 @@ from googleapiclient.http import MediaFileUpload
 
 BACKUP_DB_FORMAT = getattr(settings, 'BACKUP_DB_FORMAT', 'json')
 
+service_account_file = os.path.join(settings.BASE_DIR, 'gdrive_account.json')
+scopes = ['https://www.googleapis.com/auth/drive']
+credentials = Credentials.from_service_account_file(service_account_file, scopes=scopes)
+service = build('drive', 'v3', credentials=credentials)
+
 
 def backup_db():
     with tempfile.NamedTemporaryFile() as tmp_file:
         management.call_command('dumpdata', indent=2, format=BACKUP_DB_FORMAT, output=tmp_file.name)
         result = _upload_file_to_gdrive(tmp_file.name)
-        return result
+
+    _delete_old_files()
+
+    return result
 
 
 def _upload_file_to_gdrive(file_path: str) -> Dict[str, str]:
@@ -27,10 +35,20 @@ def _upload_file_to_gdrive(file_path: str) -> Dict[str, str]:
         'parents': [settings.GDRIVE_DIR_ID]
     }
 
-    service_account_file = os.path.join(settings.BASE_DIR, 'gdrive_account.json')
-    scopes = ['https://www.googleapis.com/auth/drive']
-    credentials = Credentials.from_service_account_file(service_account_file, scopes=scopes)
-    service = build('drive', 'v3', credentials=credentials)
     media = MediaFileUpload(file_path, resumable=True)
     result = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
     return result
+
+
+def _delete_old_files():
+    result = service.files().list(
+        fields='files(id, name)',
+        orderBy='name desc',
+        pageSize=1000,
+        q=f'"{settings.GDRIVE_DIR_ID}" in parents'
+    ).execute()
+
+    backup_db_file_number = getattr(settings, 'BACKUP_DB_FILE_NUMBER', 30)
+    old_files = result['files'][backup_db_file_number:]
+    for file in old_files:
+        service.files().delete(fileId=file['id']).execute()
--- backend/app/tests/test_backup_service.py
+++ backend/app/tests/test_backup_service.py
@@ -12,7 +12,8 @@ class BackupServiceTests(TestCase):
     @patch('tempfile._mkstemp_inner')
     @patch('django.core.management.call_command')
     @patch('app.services.backup_service._upload_file_to_gdrive')
-    def test_backup_db_with_fake_functions(self, upload_file_to_gdrive, call_command, mock_tmp_file_name):
+    @patch('app.services.backup_service._delete_old_files')
+    def test_backup_db_with_fake_functions(self, delete_old_files, upload_file_to_gdrive, call_command, mock_tmp_file_name):
         tmp_file_path = '/tmp/wildrace_test_db.json'
         tmp_file = os.open(tmp_file_path, tempfile._bin_openflags, 0o600)
         mock_tmp_file_name.return_value = (tmp_file, tmp_file_path)
@@ -25,8 +26,12 @@ class BackupServiceTests(TestCase):
         self.assertEqual(upload_file_to_gdrive.call_count, 1)
         self.assertEqual(upload_file_to_gdrive.call_args.args[0], tmp_file_path)
         self.assertFalse(os.path.exists(tmp_file_path))
+        self.assertEqual(delete_old_files.call_count, 1)
 
     def test_backup_db(self):
         result = backup_service.backup_db()
         self.assertIsInstance(result['id'], str)
         self.assertEquals(len(result['id']), 33)
+
+    def test_delete_old_files(self):
+        backup_service._delete_old_files()
