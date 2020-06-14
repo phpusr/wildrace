e4ab518ee6ef3ed0c9cd8bef8b7646a88f6ef5db
phpusr <phpusr@gmail.com>
2020-06-14 14:14:30 +0500

    WR-35: Added wait for DB script

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-celery worker -A tasks -B --scheduler django &
-daphne app.asgi:application -b 0.0.0.0 -p $1
+./manage.py wait_for_db && celery worker -A tasks -B --scheduler django &
+./manage.py wait_for_db && daphne app.asgi:application -b 0.0.0.0 -p $1
