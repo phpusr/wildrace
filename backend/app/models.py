4f563877bfacdbb3de5f86d5e37f416dc44f5b70
phpusr <phpusr@gmail.com>
2024-10-21 13:35:53 +0500

    Increase url size

--- backend/app/models.py
+++ backend/app/models.py
@@ -63,45 +63,45 @@ class Profile(models.Model):
 
     has_photo = models.BooleanField(null=True, blank=True)
 
-    photo_50 = models.URLField(blank=True)
+    photo_50 = models.URLField(blank=True, max_length=500)
     """
     url квадратной фотографии пользователя, имеющей ширину 50 пикселей.
     В случае отсутствия у пользователя фотографии возвращается http://vk.com/images/camera_c.gif
     """
 
-    photo_100 = models.URLField(blank=True)
+    photo_100 = models.URLField(blank=True, max_length=500)
     """
     url квадратной фотографии пользователя, имеющей ширину 100 пикселей.
     В случае отсутствия у пользователя фотографии возвращается http://vk.com/images/camera_b.gif
     """
 
-    photo_200 = models.URLField(blank=True)
+    photo_200 = models.URLField(blank=True, max_length=500)
     """
     url квадратной фотографии пользователя, имеющей ширину 200 пикселей.
     Если фотография была загружена давно, изображения с такими размерами может не быть,
     в этом случае ответ не будет содержать этого поля
     """
 
-    photo_200_orig = models.URLField(blank=True)
+    photo_200_orig = models.URLField(blank=True, max_length=500)
     """
     url фотографии пользователя, имеющей ширину 200 пикселей.
     В случае отсутствия у пользователя фотографии возвращается http://vk.com/images/camera_a.gif
     """
 
-    photo_400_orig = models.URLField(blank=True)
+    photo_400_orig = models.URLField(blank=True, max_length=500)
     """
     url фотографии пользователя, имеющей ширину 400 пикселей.
     Если у пользователя отсутствует фотография такого размера, ответ не будет содержать этого поля
     """
 
-    photo_max = models.URLField(blank=True)
+    photo_max = models.URLField(blank=True, max_length=500)
     """
     url квадратной фотографии пользователя с максимальной шириной.
     Может быть возвращена фотография, имеющая ширину как 200, так и 100 пикселей.
     В случае отсутствия у пользователя фотографии возвращается http://vk.com/images/camera_b.gif
     """
 
-    photo_max_orig = models.URLField(blank=True)
+    photo_max_orig = models.URLField(blank=True, max_length=500)
     """
     url фотографии пользователя максимального размера.
     Может быть возвращена фотография, имеющая ширину как 400, так и 200 пикселей.
