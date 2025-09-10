7c4f4c2031e604e555c29b97f64b986a9f7d1066
phpusr <phpusr@gmail.com>
2025-09-10 19:41:46 +0500

    Disabled custom path frontend build

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,5 @@
-const path = require("path")
-
 module.exports = {
-    outputDir: path.resolve(__dirname, '../backend/app/static/front'),
+    //outputDir: path.resolve(__dirname, '../backend/app/static/front'),
     transpileDependencies: [
         "vuetify"
     ]
